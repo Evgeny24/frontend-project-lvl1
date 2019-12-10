6fc5acefc4bf4ec5dfa1ca4f3d0f9d4c8a16f143
Evgeny Lobachev <evgeny@sigmaltd.ru>
2019-12-10 17:52:39 +0300

    Step3 - add asking user name

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,3 +1,8 @@
 #!/usr/bin/env node
 
+import { getUserName } from '..';
+
 console.log("Welcome to the Brain Games!");
+const userName = getUserName();
+const greet = `Hello, ${userName}!`;
+console.log(greet);
---
+++ src/index.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+
+import readlineSync from 'readline-sync';
+
+export const getUserName = () => {
+  const userName = readlineSync.question('May I have your name? ');
+  return userName;
+};
